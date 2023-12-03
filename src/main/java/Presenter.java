dc52deb91972d74fc15609923b91faeb8b286fe8
Takashi Sasaki <tsasaki609@gmail.com>
2023-12-03 13:10:21 +0900

    adding

--- src/main/java/Presenter.java
+++ src/main/java/Presenter.java
@@ -5,6 +5,7 @@ public class Presenter {
 	private Latitude latitude;
 	private MapLongitude mapLongitude;
 	private MapLatitude mapLatitude;
+	private boolean isAutoUpdate = true;
 	public Presenter(Longitude longitude, Latitude latitude, MapLongitude mapLongitude, MapLatitude mapLatitude) {
 		this.longitude = longitude;
 		this.latitude = latitude;
@@ -14,7 +15,16 @@ public class Presenter {
 	public void updateGPS(double cur_lat, double cur_long) {
 		double longitude = this.longitude.updateGPS(cur_lat, cur_long);
 		double latitude = this.latitude.updateGPS(cur_lat, cur_long);
-		this.mapLongitude.updateLongitude(longitude);
-		this.mapLatitude.updateLatitude(latitude);
+		if (isAutoUpdate) {
+			this.mapLongitude.updateLongitude(longitude);
+			this.mapLatitude.updateLatitude(latitude);
+		}
+	}
+	public void updateManually() {
+		this.mapLongitude.updateLongitude(this.longitude.getValue());
+		this.mapLatitude.updateLatitude(this.latitude.getValue());
+	}
+	public void setAutoUpdate(boolean isAutoUpdate) {
+		this.isAutoUpdate = isAutoUpdate;
 	}
 }
