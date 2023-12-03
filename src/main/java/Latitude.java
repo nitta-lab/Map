f32836e689904067f9741588df7f8a6f37d78078
Takashi Sasaki <tsasaki609@gmail.com>
2023-12-03 12:34:39 +0900

    generating

--- src/main/java/Latitude.java
+++ src/main/java/Latitude.java
@@ -1,16 +1,12 @@
 import java.util.*;
 
 public class Latitude {
-	private MapLatitude mapLatitude;
 	private double value;
-	public Latitude(MapLatitude mapLatitude) {
-		this.mapLatitude = mapLatitude;
-	}
 	public double getValue() {
 		return this.value;
 	}
-	public void updateGPS(double cur_lat, double cur_long) {
+	public double updateGPS(double cur_lat, double cur_long) {
 		this.value = cur_lat;
-		this.mapLatitude.updateLatitude(this.value);
+		return this.value;
 	}
 }
