e09642f6ce294d720bfb0c0d1ba6b3f22ba52fff
ted21 <ted199159@gmail.com>
2023-12-05 00:26:33 +0800

    Refactor

---
+++ src/main/java/Presenter.java
@@ -0,0 +1,20 @@
+public class Presenter {
+	private Longitude longitude;
+	private Latitude latitude;
+	private MapLongitude mapLongitude;
+	private MapLatitude mapLatitude;
+
+    public Presenter(MapLongitude mapLongitude,Longitude longitude,MapLatitude mapLatitude,Latitude latitude) {
+        this.mapLongitude = mapLongitude;
+        this.longitude = longitude;
+        this.mapLatitude = mapLatitude;
+        this.latitude = latitude;
+    }
+    
+	public void updateGPS(double cur_lat, double cur_long) {
+		double longitude = this.longitude.updateGPS(cur_lat, cur_long);
+		double latitude = this.latitude.updateGPS(cur_lat, cur_long);
+		this.mapLongitude.updateLongitude(longitude);
+		this.mapLatitude.updateLatitude(latitude);
+	}
+}
