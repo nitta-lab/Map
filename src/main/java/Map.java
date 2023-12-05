d21773fd685ca2848abba4b550118024f427c0e8
Joe Sakura <josephtessel@gmail.com>
2023-12-05 18:35:17 +0800

    ツール課題

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,30 +1,31 @@
 import java.util.*;
 
 public class Map {
-	private MapLongitude mapLongitude;
 	private Longitude longitude;
 	private MapLatitude mapLatitude;
+	private MapLongitude mapLongitude;
 	private Latitude latitude;
+	private Presenter presenter;
 	public Map() {
-		this.mapLongitude = new MapLongitude();
-		this.longitude = new Longitude(mapLongitude);
+		this.longitude = new Longitude();
 		this.mapLatitude = new MapLatitude();
-		this.latitude = new Latitude(mapLatitude);
-	}
-	public double getMapLongitude() {
-		return mapLongitude.getValue();
+		this.mapLongitude = new MapLongitude();
+		this.latitude = new Latitude();
+		this.presenter = new Presenter(longitude, latitude, mapLongitude, mapLatitude);
 	}
 	public double getLongitude() {
 		return longitude.getValue();
 	}
-	public void updateGPS(double cur_lat, double cur_long) {
-		this.longitude.updateGPS(cur_lat, cur_long);
-		this.latitude.updateGPS(cur_lat, cur_long);
-	}
 	public double getMapLatitude() {
 		return mapLatitude.getValue();
 	}
+	public double getMapLongitude() {
+		return mapLongitude.getValue();
+	}
 	public double getLatitude() {
 		return latitude.getValue();
+	}
+	public void updateGPS(double cur_lat, double cur_long) {
+		this.presenter.updateGPS(cur_lat, cur_long);
 	}
 }
