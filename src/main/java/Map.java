22626bece9cd3580072fb7f44860e5cbdd89ff3f
ted21 <ted199159@gmail.com>
2023-12-07 13:32:42 +0800

    Generated

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -2,29 +2,30 @@ import java.util.*;
 
 public class Map {
 	private MapLongitude mapLongitude;
-	private Longitude longitude;
-	private MapLatitude mapLatitude;
 	private Latitude latitude;
+	private MapLatitude mapLatitude;
+	private Longitude longitude;
+	private Presenter presenter;
 	public Map() {
 		this.mapLongitude = new MapLongitude();
-		this.longitude = new Longitude(mapLongitude);
+		this.latitude = new Latitude();
 		this.mapLatitude = new MapLatitude();
-		this.latitude = new Latitude(mapLatitude);
+		this.longitude = new Longitude();
+		this.presenter = new Presenter(longitude, latitude, mapLongitude, mapLatitude);
 	}
 	public double getMapLongitude() {
 		return mapLongitude.getValue();
+	}
+	public double getLatitude() {
+		return latitude.getValue();
+	}
+	public double getMapLatitude() {
+		return mapLatitude.getValue();
 	}
 	public double getLongitude() {
 		return longitude.getValue();
 	}
 	public void updateGPS(double cur_lat, double cur_long) {
-		this.longitude.updateGPS(cur_lat, cur_long);
-		this.latitude.updateGPS(cur_lat, cur_long);
-	}
-	public double getMapLatitude() {
-		return mapLatitude.getValue();
-	}
-	public double getLatitude() {
-		return latitude.getValue();
+		this.presenter.updateGPS(cur_lat, cur_long);
 	}
 }
