ffd6c443f5a5d0ac647cd1d16a5bf6ebaddd6b9e
sinkovmikhail <sinkovmikhail@outlook.com>
2023-12-14 19:51:32 +0900

    リファクタリング課題

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,15 +1,15 @@
-import java.util.*;
-
 public class Map {
 	private MapLongitude mapLongitude;
 	private Longitude longitude;
 	private MapLatitude mapLatitude;
 	private Latitude latitude;
+	private Presenter presenter;
 	public Map() {
 		this.mapLongitude = new MapLongitude();
 		this.longitude = new Longitude(mapLongitude);
 		this.mapLatitude = new MapLatitude();
 		this.latitude = new Latitude(mapLatitude);
+		this.presenter = new Presenter(mapLongitude, longitude, mapLatitude, latitude);
 	}
 	public double getMapLongitude() {
 		return mapLongitude.getValue();
@@ -18,8 +18,7 @@ public class Map {
 		return longitude.getValue();
 	}
 	public void updateGPS(double cur_lat, double cur_long) {
-		this.longitude.updateGPS(cur_lat, cur_long);
-		this.latitude.updateGPS(cur_lat, cur_long);
+		this.presenter.updateGPS(cur_lat, cur_long);
 	}
 	public double getMapLatitude() {
 		return mapLatitude.getValue();
