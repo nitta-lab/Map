3a59d1931fa7287f74799ffb46b4ca3a4223c0eb
hmasu <h.masuyama0714@gmail.com>
2023-12-15 15:03:37 +0900

    リファクタリング対応

--- src/main/java/Longitude.java
+++ src/main/java/Longitude.java
@@ -1,16 +1,10 @@
-import java.util.*;
-
 public class Longitude {
-	private MapLongitude mapLongitude;
 	private double value;
-	public Longitude(MapLongitude mapLongitude) {
-		this.mapLongitude = mapLongitude;
-	}
 	public double getValue() {
 		return this.value;
 	}
-	public void updateGPS(double cur_lat, double cur_long) {
+	public double updateGPS(double cur_lat, double cur_long) {
 		this.value = cur_long;
-		this.mapLongitude.updateLongitude(this.value);
+		return this.getValue();
 	}
 }
---
+++ src/main/java/Presenter.java
@@ -0,0 +1,18 @@
+public class Presenter {
+	private Longitude longitude;
+	private Latitude latitude;
+	private MapLongitude mapLongitude;
+	private MapLatitude mapLatitude;
+	public Presenter(Longitude longitude, Latitude latitude, MapLongitude mapLongitude, MapLatitude mapLatitude) {
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.mapLongitude = mapLongitude;
+		this.mapLatitude = mapLatitude;
+	}
+	public void updateGPS(double cur_lat, double cur_long) {
+		double longitude = this.longitude.updateGPS(cur_lat, cur_long);
+		double latitude = this.latitude.updateGPS(cur_lat, cur_long);
+		this.mapLongitude.updateLongitude(longitude);
+		this.mapLatitude.updateLatitude(latitude);
+	}
+}
