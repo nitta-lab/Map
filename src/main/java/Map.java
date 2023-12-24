b6968c27c9836f460f14f1fa440ab32914dec5e8
hashi <hashi@DESKTOP-NM2NDTU>
2023-12-24 17:20:18 +0900

    finish

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Map {
 	private MapLatitude mapLatitude;
 	private MapLongitude mapLongitude;
@@ -27,5 +25,11 @@ public class Map {
 	}
 	public void updateGPS(double cur_lat, double cur_long) {
 		this.presenter.updateGPS(cur_lat, cur_long);
+	}
+	public void setAutoUpdate(boolean isAutoUpdate) {
+		this.presenter.setAutoUpdate(isAutoUpdate);
+	}
+	public void updateManually() {
+		this.presenter.updateManually();
 	}
 }
